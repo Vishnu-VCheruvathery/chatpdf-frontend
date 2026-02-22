f8be8f093ff9f14afe74dd75fb304a5abbe7703c
Vishnu-VCheruvathery <c.vishnu.vasudevan@gmail.com>
2026-02-22 13:20:45 +0530

    fixes

--- src/pages/Chatpage.tsx
+++ src/pages/Chatpage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect} from 'react'
+import  { useEffect} from 'react'
 // import { socket } from '../socket';
 // import StreamedMarkdown from '../components/Markdown';
 // import DropFileInput from '../components/DropFileInput';
